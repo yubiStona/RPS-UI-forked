579571cc84d7444d054b71fcd7327cf4221e9c85
Ankit Belal <aankitbelal@gmail.com>
2025-08-17 08:18:49 +0545

    remove the boiler plate code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,16 @@
 <script setup>
 defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
+  // msg: {
+  //   type: String,
+  //   required: true,
+  // },
 })
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+  <div class="flex flex-col items-center justify-center min-h-screen">
+    <h1 class="font-semibold text-4xl text-center mt-2 mb-4">
+      this is the Result processing System frontend made in vue
+    </h1>
   </div>
 </template>
-
-<style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
-}
-
-h3 {
-  font-size: 1.2rem;
-}
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
-}
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
-}
-</style>
