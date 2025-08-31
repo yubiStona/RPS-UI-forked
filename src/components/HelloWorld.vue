cc4d09a7e038da0d470e1d1ad38016f48dd49a67
Ankit Belal <aankitbelal@gmail.com>
2025-08-31 14:22:31 +0545

    changed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,12 +5,19 @@ defineProps({
   //   required: true,
   // },
 })
+import { ref, computed } from 'vue'
+const name = ref('name here')
+const uppercasedName = computed(() => name.value.toUpperCase())
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center min-h-screen">
     <h1 class="font-semibold text-4xl text-center mt-2 mb-4">
-      this is the Result processing System frontend made in vue
+      this is the Result processing System frontend made in vue the dynamic system for the result
+      processing and report generation
     </h1>
+
+    <input type="text" v-model="name" />
+    <h1>{{ uppercasedName }}</h1>
   </div>
 </template>
