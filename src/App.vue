813671938fccba9c2fa34d42eda7607c3064a7fc
Yubraj Dhungana <yubraj.dhungana21@gmail.com>
2025-09-01 13:15:24 +0545

    refactor: update index.html structure, add login component, and enhance styling

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/login.vue'
 </script>
-
 <template>
-  <HelloWorld />
+  <Login />
 </template>
